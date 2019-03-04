e17fcf1e7eeb0c8f0daee2e9ef1c31cc12fc6309
marin <34997944+mbizov@users.noreply.github.com>
2019-03-04 22:03:43 +0200

    updated

--- tinyunit.h
+++ tinyunit.h
@@ -7,7 +7,7 @@ extern int tu_tests_passed;
 
 #define tu_assert(test, msg) do { if (!(test)) return msg; } while (0)
 #define tu_run_test(test) do { \
-                            char *msg = test(); \
+                            char *msg = test; \
                             tu_tests_run++; \
                             if (msg) return msg; \
                             tu_tests_passed++; \
